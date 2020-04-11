80370b531d62ab2925ec8a269a7bb83504696349
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-12 00:18:39 +0300

    [Lib] Add Theme info and repository

---
+++ SetThemeLib/Theme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetThemeLib
+{
+    public class Theme
+    {
+        public string Value { get; private set; }
+        public string Description { get; private set; }
+        public Action<IntPtr> ExtraAction { get; private set; }
+
+        public Theme(string value, string description, Action<IntPtr> extraAction = null)
+        {
+            Value = value;
+            Description = description;
+            ExtraAction = extraAction;
+        }
+    }
+}
---
+++ SetThemeLib/ThemesRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetThemeLib
+{
+    public class ThemesRepository
+    {
+        private static void ApplyDark32(IntPtr hwnd)
+        {
+
+        }
+
+        public static Theme[] Themes = {
+            new Theme(null, "Supposed to be a default theming"),
+            new Theme(" ", "Classic theming"),
+            new Theme("Explorer", "Gives list-view control the appearance of a Windows Explorer list"),
+            new Theme("DarkMode_Explorer", "Seems to be dark variation of Explorer theme"),
+            new Theme("Explorer", "Explorer + apply Win32 dark theme", ApplyDark32),
+            new Theme("DarkMode_Explorer", "DarkMode_Explorer + apply Win32 dark theme", ApplyDark32),
+        };
+    }
+}
