9cfccdcdf032206b2035d373aeb62676e6be5542
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-12 02:17:43 +0300

    [HwndSearch] Implement search through processes + onFail event

--- SetThemeLib/HwndSearch.cs
+++ SetThemeLib/HwndSearch.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SetThemeLib
@@ -10,7 +12,7 @@ namespace SetThemeLib
         /// <summary>
         /// Searches HWNDs and invokes Action
         /// </summary>
-        public static void ForEach(HwndSearchTypes type, object[] valuesRaw, Action<IntPtr> action)
+        public static void ForEach(HwndSearchTypes type, object[] valuesRaw, Action<IntPtr> action, Action<IntPtr, Exception> onFail = null)
         {
             if (type == HwndSearchTypes.Specific)
             {
@@ -26,8 +28,43 @@ namespace SetThemeLib
                     action(mainHwnd);
                 else if (type == HwndSearchTypes.ForProcess)
                 {
-                    // check for process name
-                    throw new NotImplementedException();
+                    uint proc;
+                    User32.GetWindowThreadProcessId(mainHwnd, out proc);
+
+                    IntPtr procHandle = Kernel32.OpenProcess(Kernel32.ProcessAccessFlags.QueryInformation | Kernel32.ProcessAccessFlags.VirtualMemoryRead, false, (int)proc);
+
+                    if (procHandle == IntPtr.Zero)
+                    {
+                        var error = Marshal.GetLastWin32Error();
+
+                        if (error == 5) // Referencing to original SetClassicTheme we should ignore this error
+                            return true; 
+
+                        onFail?.Invoke(mainHwnd, new Exception("Win32 error when OpenProcess: " + Marshal.GetLastWin32Error()));
+                        return true;
+                    }
+
+
+                    StringBuilder moduleName = new StringBuilder(1024);
+                    var moduleResult = PsApi.GetModuleFileNameEx(procHandle, IntPtr.Zero, moduleName, moduleName.Capacity);
+
+                    if (moduleResult == 0)
+                    {
+                        onFail?.Invoke(mainHwnd, new Exception("Failed to get module file name: " + moduleResult));
+                        Kernel32.CloseHandle(procHandle);
+                        return true;
+                    }
+
+                    Kernel32.CloseHandle(procHandle);
+
+                    var moduleFile = new FileInfo(moduleName.ToString());
+
+                    if (!valuesRaw.Select(x => x.ToString().ToLower()).Contains(moduleFile.Name.ToLower()))
+                    {
+                        return true;
+                    }
+
+                    action(mainHwnd);
                 }
                 else throw new NotImplementedException("Unknown search type: " + type);
 
