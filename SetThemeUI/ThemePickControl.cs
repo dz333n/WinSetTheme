234261bfa7645cc83be29ff522ba205b92d272af
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-12 00:33:29 +0300

    [ThemePickControl] Picker implementation

--- SetThemeUI/ThemePickControl.cs
+++ SetThemeUI/ThemePickControl.cs
@@ -12,11 +12,41 @@ namespace SetThemeUI
 {
     public partial class ThemePickControl : UserControl
     {
-        public Theme SelectedTheme => throw new NotImplementedException();
+        public Theme SelectedTheme
+        {
+            get
+            {
+                var item = comboBox1.SelectedItem;
+                if (item != null && item is Theme)
+                    return (Theme)item;
+                else
+                    return null;
+            }
+        }
 
         public ThemePickControl()
         {
             InitializeComponent();
+
+            foreach (var theme in ThemesRepository.Themes)
+                comboBox1.Items.Add(theme);
+
+            comboBox1.SelectedItem = comboBox1.Items[0];
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var theme = SelectedTheme;
+
+            if (theme != null)
+                labelDescription.Text = theme.Description;
+            else
+                labelDescription.Text = "Select a theme";
         }
     }
 }
