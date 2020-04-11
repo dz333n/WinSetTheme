b64eac0e03f2f5e6215c5370a416eb459c9447e1
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-11 22:01:51 +0300

    Begin .NET implementation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# SetClassicTheme [![Build status](https://ci.appveyor.com/api/projects/status/6faaky9mup279p03?svg=true)](https://ci.appveyor.com/project/feel-the-dz3n/setclassictheme)
-This program uses ``SetWindowTheme`` function to disable window theming.
+# SetTheme [![Build status](https://ci.appveyor.com/api/projects/status/6faaky9mup279p03?svg=true)](https://ci.appveyor.com/project/feel-the-dz3n/setclassictheme)
+This program uses ``SetWindowTheme`` function. 
 
 ![img](https://raw.githubusercontent.com/feel-the-dz3n/SetClassicTheme/master/sctprev.gif)
 
---
+++ SetThemeLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SetThemeLib
+{
+    public class UxTheme
+    {
+        [DllImport("uxtheme.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        public static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+
+        [DllImport("uxtheme.dll", EntryPoint = "#133")]
+        public static extern bool AllowDarkModeForWindow(IntPtr a_HWND, bool a_Allow);
+    }
+}
