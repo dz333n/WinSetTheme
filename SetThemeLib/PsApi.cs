b0c9209fff744578022b92f2013f32072b0e46e1
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-12 02:17:24 +0300

    Few new api functions

---
+++ SetThemeLib/PsApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SetThemeLib
+{
+    public class PsApi
+    {
+        [DllImport("psapi.dll")]
+        public static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In] [MarshalAs(UnmanagedType.U4)] int nSize);
+    }
+}
