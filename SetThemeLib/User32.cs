12779735a91487113e6532ae24c6d6e7260e8cd0
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-12 02:17:24 +0300

    Few new api functions

--- SetThemeLib/User32.cs
+++ SetThemeLib/User32.cs
@@ -17,5 +17,12 @@ namespace SetThemeLib
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumChildWindows(IntPtr hwndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        // When you don't want the ProcessId, use this overload and pass IntPtr.Zero for the second parameter
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
     }
 }
