41f9febc2ff598ef7d3c84c36e3d6def78c66194
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-11 22:19:48 +0300

    Add LogControl, change form name

--- SetThemeUI/LogControl.Designer.cs
+++ SetThemeUI/LogControl.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace SetThemeUI
 {
-    partial class MainWindow
+    partial class LogControl
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,28 +20,39 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.tbLog = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // MainWindow
+            // tbLog
+            // 
+            this.tbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLog.Location = new System.Drawing.Point(0, 0);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.Size = new System.Drawing.Size(150, 150);
+            this.tbLog.TabIndex = 1;
+            // 
+            // LogControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 441);
-            this.Name = "MainWindow";
-            this.Text = "SetTheme";
+            this.Controls.Add(this.tbLog);
+            this.Name = "LogControl";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        public System.Windows.Forms.TextBox tbLog;
     }
 }
-
