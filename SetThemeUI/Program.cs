fcd3a9d7b980159753b3e380da5dc453f3f3bab0
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-11 22:03:26 +0300

    Form1 -> MainWindow

--- SetThemeUI/Program.cs
+++ SetThemeUI/Program.cs
@@ -15,7 +15,7 @@ namespace SetThemeUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
