91b558b8bb5d9ce1369d4fde24c2958c10e652be
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-12 00:21:33 +0300

    [MainWnd] Update layout

--- SetThemeUI/MainWindow.Designer.cs
+++ SetThemeUI/MainWindow.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnPerform = new System.Windows.Forms.Button();
-            this.log = new SetThemeUI.LogControl();
             this.hwndSearchControl1 = new SetThemeUI.HwndSearchControl();
             this.themePickControl1 = new SetThemeUI.ThemePickControl();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.log = new SetThemeUI.LogControl();
+            this.btnPerform = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
@@ -52,51 +52,15 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.54683F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(550, 173);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(550, 155);
             this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // tableLayoutPanel2
-            // 
-            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.log, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.btnPerform, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 191);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.77698F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.22302F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(550, 139);
-            this.tableLayoutPanel2.TabIndex = 2;
-            // 
-            // btnPerform
-            // 
-            this.btnPerform.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnPerform.Location = new System.Drawing.Point(3, 3);
-            this.btnPerform.Name = "btnPerform";
-            this.btnPerform.Size = new System.Drawing.Size(544, 33);
-            this.btnPerform.TabIndex = 1;
-            this.btnPerform.Text = "Perform SetWindowTheme for each HWND";
-            this.btnPerform.UseVisualStyleBackColor = true;
-            this.btnPerform.Click += new System.EventHandler(this.btnPerform_Click);
-            // 
-            // log
-            // 
-            this.log.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.log.Location = new System.Drawing.Point(3, 42);
-            this.log.Name = "log";
-            this.log.Size = new System.Drawing.Size(544, 94);
-            this.log.TabIndex = 0;
             // 
             // hwndSearchControl1
             // 
             this.hwndSearchControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.hwndSearchControl1.Location = new System.Drawing.Point(3, 3);
             this.hwndSearchControl1.Name = "hwndSearchControl1";
-            this.hwndSearchControl1.Size = new System.Drawing.Size(269, 167);
+            this.hwndSearchControl1.Size = new System.Drawing.Size(269, 149);
             this.hwndSearchControl1.TabIndex = 0;
             // 
             // themePickControl1
@@ -104,15 +68,50 @@
             this.themePickControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.themePickControl1.Location = new System.Drawing.Point(278, 3);
             this.themePickControl1.Name = "themePickControl1";
-            this.themePickControl1.Size = new System.Drawing.Size(269, 167);
+            this.themePickControl1.Size = new System.Drawing.Size(269, 149);
             this.themePickControl1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.log, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnPerform, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 173);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(550, 227);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // log
+            // 
+            this.log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.log.Location = new System.Drawing.Point(3, 43);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(544, 181);
+            this.log.TabIndex = 0;
+            // 
+            // btnPerform
+            // 
+            this.btnPerform.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPerform.Location = new System.Drawing.Point(3, 3);
+            this.btnPerform.Name = "btnPerform";
+            this.btnPerform.Size = new System.Drawing.Size(544, 34);
+            this.btnPerform.TabIndex = 1;
+            this.btnPerform.Text = "Perform SetWindowTheme for each HWND";
+            this.btnPerform.UseVisualStyleBackColor = true;
+            this.btnPerform.Click += new System.EventHandler(this.btnPerform_Click);
             // 
             // MainWindow
             // 
             this.AcceptButton = this.btnPerform;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 342);
+            this.ClientSize = new System.Drawing.Size(574, 412);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainWindow";
