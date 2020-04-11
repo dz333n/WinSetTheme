5226526de2f2c032cd2b5fb6d7f002a6729b7797
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-11 23:29:40 +0300

    [LogCtrl] Add scroll bar

--- SetThemeUI/LogControl.Designer.cs
+++ SetThemeUI/LogControl.Designer.cs
@@ -37,6 +37,7 @@
             this.tbLog.Location = new System.Drawing.Point(0, 0);
             this.tbLog.Multiline = true;
             this.tbLog.Name = "tbLog";
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.tbLog.Size = new System.Drawing.Size(150, 150);
             this.tbLog.TabIndex = 1;
             // 
