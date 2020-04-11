aab99cb343a43e4f651e60c70bcf99d1912e720b
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-11 22:51:05 +0300

    Add HwndSearchControl and types

---
+++ SetThemeLib/HwndSearchTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetThemeLib
+{
+    public enum HwndSearchTypes
+    {
+        All,
+        ForProcess,
+        Specific
+    }
+}
